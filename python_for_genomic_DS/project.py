d5b258daec67bdbe24038212066de0df0ff3a8f9
Raúl Miñán <raulminan@gmail.com>
2022-04-25 21:46:21 +0200

    Feat: Add orf_finder

--- python_for_genomic_DS/project.py
+++ python_for_genomic_DS/project.py
@@ -51,24 +51,66 @@ class DNAToolKit():
         """
         max_len = 0
         min_len = 1e100
-        for k, v in self.dict.items():
-            if len(v) > max_len:
-                max_len = len(v)
-                max_len_id = k
-            if len(v) < min_len:
-                min_len = len(v)
-                min_len_id = k
+        for identifier, sequence in self.dict.items():
+            if len(sequence) > max_len:
+                max_len = len(sequence)
+                max_len_id = identifier
+            if len(sequence) < min_len:
+                min_len = len(sequence)
+                min_len_id = identifier
                 
         print(f"Max length: {max_len}")
         print(f"Min length: {min_len}")
         print(f"Identifier with max_len: {max_len_id}")
         print(f"Identifier with min_len: {min_len_id}")
         
-    def orf_finder(self, frame):
-        """Finds all ORFs present in each sequence of the FASTA file
+    def orf_finder(self, sequence, frame):
+        """Finds all open reading frames (ORFs) present in each sequence of the 
+        FASTA file. 
 
         Args:
-            frame (int): Reading frame used. Must be 1, 2 or 3.
+            sequence (str): sequence to find the ORFs in
+            frame (int): Reading frame used to find ORFs. Must be 1, 2 or 3.
+
+        Returns:
+            Longest ORF, its length and its position
         """
-                            
+        if frame not in [1, 2, 3]:
+            raise ValueError("Frame must be 1, 2 or 3")
         
+        start = "ATG"
+        stop = ["TAA", "TAG", "TGA"]
+        
+        max_orf_len = 0
+        max_orf_start = 0
+        max_orf = "" 
+        temp_max_orf = "" # to store the temporary ORFs
+        sequence = sequence[frame - 1:]
+        
+        for i in range(0, len(sequence), 3):
+            if sequence[i:i+3] == start:
+                for j in range(i, len(sequence), 3):
+                    codon = sequence[j:j+3]
+                    temp_max_orf += codon #add current codon to the ORF sequence
+                    orf_len = len(sequence[i:j+3]) 
+                    if codon in stop:
+                        if orf_len > max_orf_len:
+                            max_orf_len = orf_len
+                            max_orf_start = i
+                            max_orf = temp_max_orf
+                            temp_max_orf = ""
+                        break
+        
+        if max_orf == "":
+            print("No ORF found")                 
+        else:
+            print(f"longest orf is {max_orf}, it has a length of {max_orf_len} and starts at position {max_orf_start}")
+            return max_orf, max_orf_len, max_orf_start
+        
+        
+
+                    
+      
+        
+
+    
